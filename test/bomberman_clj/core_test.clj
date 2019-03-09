8ebb7f55604cecda9376c9fd2a3d9cdf946e57db
idleherb <idleherb@posteo.org>
2019-03-09 19:17:14 +0100

    Initial commit

---
+++ test/bomberman_clj/core_test.clj
@@ -0,0 +1,7 @@
+(ns bomberman-clj.core-test
+  (:require [clojure.test :refer :all]
+            [bomberman-clj.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
