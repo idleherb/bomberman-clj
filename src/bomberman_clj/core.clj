2833c1a00206e6489b75da0bd0cd66d4dcc30987
idleherb <idleherb@posteo.org>
2019-03-11 00:37:02 +0100

    Daily merge (#1)
    * Update license
    
    * Housekeeping
    
    * Update tests
    
    * Fix tests
    
    * Add arena rows
    
    * Add arena columns (placeholders only)
    
    * Refactor arena initialization
    
    * Add width and height params to init-arena
    
    * Housekeeping
    
    * Add more tests
    
    * Change arena into map
    
    * Add :players to arena
    
    * Add fixed player coordinates to arena
    
    * Housekeeping
    
    * Update grid to be a single vector
    
    * Add player spawning
    
    * Refine spawning
    
    * Housekeeping
    
    * Update init area to work with new grid and players
    
    * Add dev rendering for arena grid
    
    * Update spawn to only use empty cells

--- src/bomberman_clj/core.clj
+++ src/bomberman_clj/core.clj
@@ -1,6 +1,61 @@
 (ns bomberman-clj.core
   (:gen-class))
 
+(defn cell-at
+  "Return the cell of a grid at the given coordinates"
+  [{:keys [width height v] :as grid} coords]
+  (let [[x y] coords]
+    (nth v (+ (* x height) y))))
+
+(defn cell-empty?
+  "Check if a given cell is empty"
+  [grid coords]
+  (nil? (cell-at grid coords)))
+
+(defn rand-coords
+  "Return random coordinates within the given grid"
+  [{:keys [width height v] :as grid}]
+  [(rand-int width) (rand-int height)])
+
+(defn find-empty-cell
+  "Find a random empty cell within the given grid. Defaults to 100 tries."
+  ([grid] (find-empty-cell grid 100))
+  ([{:keys [width height v] :as grid} max-tries]
+    (loop [coords (rand-coords grid)
+            num-tries 1]
+      (if (cell-empty? grid coords)
+        (do
+          (println "D core::find-empty-cell - took" num-tries "tries")
+          coords)
+        (if (= max-tries num-tries)
+          (throw (Exception. "failed to find empty cell"))
+          (recur (rand-coords grid) (inc num-tries)))))))
+
+(defn spawn
+  "Spawn an object at the given coordinates."
+  [{:keys [width height v] :as grid} {:keys [symbol coords] :as object}]
+  (let [[x y] coords]
+    (if (not (cell-empty? grid coords))
+      (throw (Exception. "can only spawn in empty cell"))
+      (assoc grid :v (assoc v (+ (* x height) y) symbol)))))
+
+(defn init-arena
+  "Initialize a new (width x height) arena with given players placed"
+  [width height players]
+  (let [grid {:width width, :height height, :v (into (vector) (take (* width height) (repeat nil)))}]
+    (loop [grid grid
+           players players
+           player-idx 0]
+      (if (= player-idx (count players))
+        {:grid grid, :players players}
+        (let [coords (find-empty-cell grid)
+              {player-symbol :symbol} (nth players player-idx)
+              player {:symbol player-symbol, :coords coords}
+              players (assoc players player-idx player)
+              grid (spawn grid player)
+              player-idx (inc player-idx)]
+          (recur grid players player-idx))))))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
--- test/bomberman_clj/core_test.clj
+++ test/bomberman_clj/core_test.clj
@@ -2,6 +2,73 @@
   (:require [clojure.test :refer :all]
             [bomberman-clj.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest test-core
+  (testing "An empty (17 x 15) arena with 1 player should be initialized"
+    (let [width 1
+          height 2
+          players [{:symbol \P} {:symbol \Q}]
+          arena (init-arena width height players)]
+      (is (not (nil? arena)))
+      (is (map? arena))
+      (is (contains? arena :grid))
+      (let [grid (:grid arena)
+            v (:v grid)]
+        (is (= (* width height) (count v)))
+        (is (= 2 (count (filter (complement nil?) v)))))
+      (is (contains? arena :players))
+      (let [players (:players arena)]
+        (is (vector? players))
+        (is (= 2 (count players)))
+        (let [player-1 (nth players 0)]
+          (is (map? player-1))
+          (is (contains? player-1 :coords))
+          ))))
+
+  (testing "2 players should spawn on the top left and the bottom right cell of a given grid"
+    (let [width 17
+          height 15
+          grid {:width width, :height height, :v (into (vector) (take (* width height) (repeat nil)))}
+          player-1 {:symbol \P, :coords [0 0]}
+          player-2 {:symbol \Q, :coords [16 14]}
+          grid (spawn grid player-1)
+          grid (spawn grid player-2)]
+      (is (= \P (nth (:v grid) 0)))
+      (is (= \Q (nth (:v grid) 254)))))
+
+  (testing "Grid cells should be identified correctly"
+    (let [width 2
+          height 3
+          grid {:width width, :height height, :v [0 1 nil 3 4 5]}]
+      (is (= 0 (cell-at grid [0 0])))
+      (is (= 1 (cell-at grid [0 1])))
+      (is (cell-empty? grid [0 2]))
+      (is (= 3 (cell-at grid [1 0])))
+      (is (= 4 (cell-at grid [1 1])))
+      (is (= 5 (cell-at grid [1 2])))))
+
+  (testing "A pair of random coordinates should be found"
+    (let [width 2
+          height 3
+          grid {:width width, :height height, :v [nil nil nil nil nil nil]}
+          [x y] (rand-coords grid)]
+      (is (integer? x))
+      (is (integer? y))
+      (is (<= 0 x (- width 1)))
+      (is (<= 0 y (- height 1)))))
+
+  (testing "Grid cells should be identified correctly"
+    (let [width 2
+          height 3
+          grid {:width width, :height height, :v [0 1 nil 3 4 5]}]
+      (is (= 0 (cell-at grid [0 0])))
+      (is (= 1 (cell-at grid [0 1])))
+      (is (cell-empty? grid [0 2]))
+      (is (= 3 (cell-at grid [1 0])))
+      (is (= 4 (cell-at grid [1 1])))
+      (is (= 5 (cell-at grid [1 2])))))
+
+  (testing "An random empty cell should be found"
+    (let [width 3
+          height 3
+          grid {:width width, :height height, :v [1 1 1 1 1 1 1 nil 1]}]
+      (is (= [2 1] (find-empty-cell grid))))))
