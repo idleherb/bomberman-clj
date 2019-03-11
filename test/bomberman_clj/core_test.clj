e2bf0079e31592fc3cc005e9a31f496192a03754
idleherb <idleherb@posteo.org>
2019-03-11 19:16:41 +0100

    Add player navigation. Use map for players now.

--- test/bomberman_clj/core_test.clj
+++ test/bomberman_clj/core_test.clj
@@ -6,7 +6,7 @@
   (testing "An empty (17 x 15) arena with 1 player should be initialized"
     (let [width 1
           height 2
-          players [{:symbol \P} {:symbol \Q}]
+          players {:player-1 {:symbol \P} :player-2 {:symbol \Q}}
           arena (init-arena width height players)]
       (is (not (nil? arena)))
       (is (map? arena))
@@ -17,12 +17,11 @@
         (is (= 2 (count (filter (complement nil?) v)))))
       (is (contains? arena :players))
       (let [players (:players arena)]
-        (is (vector? players))
+        (is (map? players))
         (is (= 2 (count players)))
-        (let [player-1 (nth players 0)]
+        (let [player-1 (:player-1 players)]
           (is (map? player-1))
-          (is (contains? player-1 :coords))
-          ))))
+          (is (contains? player-1 :coords))))))
 
   (testing "2 players should spawn on the top left and the bottom right cell of a given grid"
     (let [width 17
@@ -71,4 +70,32 @@
     (let [width 3
           height 3
           grid {:width width, :height height, :v [1 1 1 1 1 1 1 nil 1]}]
-      (is (= [2 1] (find-empty-cell grid))))))
+      (is (= [2 1] (find-empty-cell grid)))))
+
+  (testing "A player can move to NESW empty cells"
+    (let [width 3
+          height 3
+          players {:player-1 {:symbol \P :coords [1 1]}}
+          arena (init-arena width height players)
+          arena (move arena :player-1 :south)
+          arena (move arena :player-1 :west)
+          arena (move arena :player-1 :north)
+          arena (move arena :player-1 :north)
+          players (:players arena)
+          {[x y] :coords} (:player-1 players)]
+      (is (= 0 x))
+      (is (= 0 y))))
+
+  (testing "Navigating from coordinates should succeed"
+    (let [coords [1 1]
+          coords (navigate coords :south)
+          coords (navigate coords :west)
+          coords (navigate coords :north)
+          coords (navigate coords :north)
+          [x y] coords]
+      (is (= 0 x))
+      (is (= 0 y))))
+
+  (testing "Navigating with invalid direction should fail"
+    (let [coords [1 1]]
+      (is (thrown-with-msg? Exception #"invalid direction:" (navigate coords :down))))))
