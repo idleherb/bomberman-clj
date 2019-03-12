087f47b0a7476eb1fac19e2de97f810a47e2b090
idleherb <idleherb@posteo.org>
2019-03-13 00:57:45 +0100

    Housekeeping

--- src/bomberman_clj/dev.clj
+++ src/bomberman_clj/dev.clj
@@ -2,22 +2,6 @@
   (:require [bomberman-clj.core :refer :all])
   (:require [lanterna.screen :as s]))
 
-(defn render-cell
-  [cell]
-  (if (nil? cell) "." cell))
-
-(defn render-arena
-  [width height players]
-  (let [arena (init-arena width height players)
-        grid (:grid arena)
-        v (:v grid)]
-    (apply str (map-indexed
-      (fn [idx cell]
-        (if (= 0 (mod (inc idx) width))
-          (str (render-cell cell) "\n")
-          (str (render-cell cell) " ")))
-      v))))
-
 (defn arena-rows
   [arena]
   (let [grid (:grid arena)
