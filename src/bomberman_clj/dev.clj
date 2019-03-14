1ad0aedd0e55271f23fc2b0b85a6bdf12708665f
idleherb <idleherb@posteo.org>
2019-03-14 21:25:58 +0100

    Add dev bomb planting

--- src/bomberman_clj/dev.clj
+++ src/bomberman_clj/dev.clj
@@ -18,18 +18,28 @@
       ))
     ))
 
-(defn key-to-player-direction
+(defn key-to-player-action
   [key]
   (case key
-    :up {:player-id :player-1 :direction :north}
-    \w {:player-id :player-2 :direction :north}
-    :right {:player-id :player-1 :direction :east}
-    \d {:player-id :player-2 :direction :east}
-    :down {:player-id :player-1 :direction :south}
-    \s {:player-id :player-2 :direction :south}
-    :left {:player-id :player-1 :direction :west}
-    \a {:player-id :player-2 :direction :west}
+    :up {:player-id :player-1, :action #(move % :player-1 :north)}
+    :right {:player-id :player-1, :action #(move % :player-1 :east)}
+    :down {:player-id :player-1, :action #(move % :player-1 :south)}
+    :left {:player-id :player-1, :action #(move % :player-1 :west)}
+    \space {:player-id :player-1, :action #(plant-bomb % :player-1)}
+    \w {:player-id :player-2, :action #(move % :player-2 :north)}
+    \d {:player-id :player-2, :action #(move % :player-2 :east)}
+    \s {:player-id :player-2, :action #(move % :player-2 :south)}
+    \a {:player-id :player-2, :action #(move % :player-2 :west)}
+    :tab {:player-id :player-2, :action #(plant-bomb % :player-2)}
     nil))
+
+(defn cell-player
+  [cell]
+  (cond
+    (nil? cell) nil
+    (not (map? cell)) (throw (Exception. (str "cell must be a map (was " (type cell) ")")))
+    :else (let [players (filter (fn [[k v]] (re-matches #"player-\d+" (name k))) cell)]
+      (first (map (fn [[_ v]] v) players)))))
 
 (defn draw-arena
   [width height players]
@@ -39,18 +49,26 @@
       (loop [arena arena
              key nil]
         (when (not= key :escape)
-          (let [{player-id :player-id, direction :direction} (key-to-player-direction key)
+          (let [{player-id :player-id, action :action} (key-to-player-action key)
                 player-id (if (nil? player-id) :player-1 player-id)
-                arena (if (not (nil? direction)) (move arena player-id direction) arena)
+                arena (if (not (nil? action)) (action arena) arena)
                 rows (arena-rows arena)
                 {[x y] :coords} (player-id (:players arena))]
             (doseq [[row-idx row] (map-indexed vector rows)]
-              (doseq [[char-idx char] (map-indexed vector row)]
+              (doseq [[cell-idx cell] (map-indexed vector row)]
                 (s/put-string scr  ; screen
-                              (if (= 0 char-idx) char-idx (* 2 char-idx))  ; x
+                              (if (= 0 cell-idx) cell-idx (* 2 cell-idx))  ; x
                               row-idx  ; y
-                              (if (nil? char) "." char)  ; string
-                              {:fg (if (nil? char) :green :white)})))  ; options
+                              (let [player (cell-player cell)]
+                                (when (not (nil? cell)) (print cell " "))
+                                (cond
+                                  (nil? cell) "."
+                                  (not (nil? player)) (:glyph player)
+                                  (contains? cell :bomb) "X"
+                                  :else (throw (Exception. (str "invalid cell content: " cell))))
+                              )  ; string
+                              {:fg (if (nil? cell) :green :white),
+                               :bg (if (contains? cell :bomb) :red :black)})))  ; options
             (s/move-cursor scr (* 2 x) y)
             (s/redraw scr)
             (recur arena (s/get-key-blocking scr))))))))
