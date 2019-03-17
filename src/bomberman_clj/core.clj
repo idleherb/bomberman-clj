39e570382fd9e293a3599e6db2a55faca9586cf3
idleherb <idleherb@posteo.org>
2019-03-18 00:52:39 +0100

    Daily merge (#7)
    * Extend and fix bomb tests
    
    * Detonating bombs now kill players and stop there
    
    * Remove player evals detonate-bomb
    
    * Housekeeping
    
    * Arena evaluation detonates expired bombs
    
    * Arena evaluation sets player hits
    
    * Add more tests
    
    * Housekeeping
    
    * Redesign :players to refer to coords only.
    
    * Redesign :bombs to refer to coords only. Also change bomb-ids.

--- src/bomberman_clj/core.clj
+++ src/bomberman_clj/core.clj
@@ -6,21 +6,48 @@
 
 (defn cell-idx
   "Return grid cell index from coordinates"
-  [{:keys [width height v], :as grid} coords]
-  (let [[x y] coords]
-    (when (and (<= 0 x (dec width))
-               (<= 0 y (dec height)))
-      (+ (* y width) x))))
+  [{:keys [width height], :as grid} [x y, :as coords]]
+  (when (and (<= 0 x (dec width))
+             (<= 0 y (dec height)))
+    (+ (* y width) x)))
+
+(defn cell-at
+  "Return the grid cell at the given coordinates"
+  [{v :v, :as grid} coords]
+  (nth v (cell-idx grid coords)))
+
+(defn assoc-grid-cell
+  ([{v :v, :as grid} coords key val]
+    (assoc grid :v
+      (assoc v (cell-idx grid coords)
+        (assoc (cell-at grid coords) key val))))
+  ([{v :v, :as grid} coords cell]
+    (assoc grid :v
+      (assoc v (cell-idx grid coords) cell))))
+
+(defn dissoc-grid-cell
+  "Like dissoc, but returns nil if the cell is empty afterwards."
+  [{v :v, :as grid} coords k]
+  (let [cell (dissoc (cell-at grid coords) k)
+        cell (if (empty? cell) nil cell)]
+    (assoc-grid-cell grid coords cell)))
 
 (defn in-grid?
   "Check if coordinates are within the given grid"
-  [{:keys [width height v], :as grid} coords]
-    (not (nil? (cell-idx grid coords))))
+  [grid coords]
+  (not (nil? (cell-idx grid coords))))
 
-(defn cell-at
-  "Return the cell of a grid at the given coordinates"
-  [{:keys [width height v], :as grid} coords]
-  (nth v (cell-idx grid coords)))
+(defn cell-player-id
+  "Checks if the given cell contains a player and returns its mapping, else nil"
+  [cell]
+  (first ; player-id (or nil)
+    (first ; first (or nil) [player-id, player]
+      (filter (fn [[k _]] (re-matches #"player-\d+" (name k))) cell))))
+
+(defn cell-player
+  "Return the player in the given cell if any"
+  ([cell] (cell-player cell (cell-player-id cell)))
+  ([cell player-id] (player-id cell)))
 
 (defn cell-empty?
   "Check if a given cell is empty"
@@ -29,13 +56,13 @@
 
 (defn rand-coords
   "Return random coordinates within the given grid"
-  [{:keys [width height v], :as grid}]
+  [{:keys [width height], :as grid}]
   [(rand-int width) (rand-int height)])
 
 (defn find-empty-cell
   "Find a random empty cell within the given grid. Defaults to 100 tries."
   ([grid] (find-empty-cell grid 100))
-  ([{:keys [width height v], :as grid} max-tries]
+  ([grid max-tries]
     (loop [coords (rand-coords grid)
            num-tries 1]
       (if (cell-empty? grid coords)
@@ -46,10 +73,13 @@
 
 (defn spawn
   "Spawn an object at the given coordinates."
-  [{:keys [width height v], :as grid}, object-id, object coords]
+  [{v :v, :as grid}
+   object-id
+   object
+   coords]
   (if (not (cell-empty? grid coords))
     (throw (Exception. "can only spawn in empty cell"))
-    (assoc grid :v (assoc v (cell-idx grid coords) {object-id object}))))
+    (assoc-grid-cell grid coords object-id object)))
 
 (defn init-arena
   "Initialize a new (width x height) arena with given players placed"
@@ -61,27 +91,26 @@
            players players
            player-idx 1]
       (if (> player-idx (count players))
-        {:grid grid, :players players, :bombs {}}
-        (if (contains? ((keyword (str "player-" player-idx)) players) :coords)
-          ; spawn player at given coords
-          (let [player-id (keyword (str "player-" player-idx))
-                {coords :coords, :as player} (player-id players)
-                grid (spawn grid player-id player coords)
-                player-idx (inc player-idx)]
-            (recur grid players player-idx))
-          ; spawn player at random coords
-          (let [coords (find-empty-cell grid)
-                player-id (keyword (str "player-" player-idx))
-                {player-glyph :glyph, :as player} (player-id players)
-                player (assoc player :coords coords)
-                players (assoc players player-id player)
-                grid (spawn grid player-id player coords)
-                player-idx (inc player-idx)]
-            (recur grid players player-idx)))))))
+        {:bombs {}
+         :grid grid
+         :players (into {} (map (fn [[k v]] [k (:coords v)])) players)}
+        (let [player-id (keyword (str "player-" player-idx))]
+          (if (contains? (player-id players) :coords)
+            ; spawn player at given coords
+            (let [{coords :coords, :as player} (player-id players)
+                  grid (spawn grid player-id (dissoc player :coords) coords)
+                  player-idx (inc player-idx)]
+              (recur grid players player-idx))
+            ; spawn player at random coords
+            (let [coords (find-empty-cell grid)
+                  grid (spawn grid player-id (player-id players) coords)
+                  players (assoc players player-id {:coords coords})
+                  player-idx (inc player-idx)]
+              (recur grid players player-idx))))))))
 
 (defn navigate
   "Navigate from coordinates into the given direction"
-  [[x y] direction]
+  [[x y, :as coords] direction]
   (case direction
     :north [x (dec y)]
     :east [(inc x) y]
@@ -93,20 +122,17 @@
   "Try to move a player in the given direction"
   [arena player-id direction]
   (let [{{v :v, :as grid} :grid, players :players} arena
-        {coords :coords, glyph :glyph, :as player} (player-id players)
+        coords (player-id players)
+        player (player-id (cell-at grid coords))
         new-coords (navigate coords direction)]
     (if (and (in-grid? grid new-coords)
              (cell-empty? grid new-coords))
-      (let [player (assoc player :coords new-coords)
-            players (assoc players player-id player)
-            v (:v grid)
-            prev-cell (cell-at grid coords)
-            prev-cell (dissoc prev-cell player-id)
-            prev-cell (if (empty? prev-cell) nil prev-cell)
-            grid (assoc grid :v (assoc v (cell-idx grid coords) prev-cell))
+      (let [grid (dissoc-grid-cell grid coords player-id)
             grid (spawn grid player-id player new-coords)
-            arena (assoc arena :grid grid)
-            arena (assoc arena :players players)]
+            players (assoc players player-id new-coords)
+            arena (assoc arena
+              :grid grid
+              :players players)]
         arena)
       arena)))
 
@@ -114,15 +140,13 @@
   "Try to plant a bomb with the given player at their current coordinates"
   [arena player-id]
   (let [{{v :v, :as grid} :grid, players :players, bombs :bombs} arena
-        {[x y, :as coords] :coords, :as player} (player-id players)
-        cell-idx (cell-idx grid coords)
-        cell (cell-at grid coords)
-        bomb {:timestamp (System/currentTimeMillis)}
-        bomb-cell (assoc cell :bomb bomb)
-        bombs (assoc bombs (keyword (str "x" x "y" y)) (assoc bomb :coords coords))]
+        [x y, :as coords] (player-id players)
+        bomb {:timestamp (System/currentTimeMillis)}  ; TODO: make timestamp parameter
+        bomb-id (keyword (str "bomb-x" x "y" y))
+        bombs (assoc bombs bomb-id coords)]
     (assoc arena
       :bombs bombs
-      :grid (assoc grid :v (assoc v cell-idx bomb-cell)))))
+      :grid (assoc-grid-cell grid coords bomb-id bomb))))
 
 (defn spread-fire
   "Spread fire along x or y axis"
@@ -131,49 +155,73 @@
    transform-coords
    radius]
   (loop [[cur-x cur-y] coords
-         {v :v, width :width, height :height, :as grid} grid]
-    (if (or (= radius (Math/abs (- cur-x x)))
+         {:keys [v width height], :as grid} grid
+         break false]
+    (if (or (true? break)
+            (= radius (Math/abs (- cur-x x)))
             (= radius (Math/abs (- cur-y y)))
             (= cur-x -1)
             (= cur-x width)
             (= cur-y -1)
             (= cur-y height))
       grid
-      (recur
-        (transform-coords [cur-x cur-y])
-        (assoc grid :v
-          (assoc v (cell-idx grid [cur-x cur-y]) {:fire nil}))))))
+      (let [cell (cell-at grid [cur-x cur-y])
+            player-id (cell-player-id cell)]
+        (recur
+          (transform-coords [cur-x cur-y])
+          (assoc-grid-cell grid [cur-x cur-y] :fire true)
+          (not (nil? player-id)))))))
 
 (defn detonate-bomb
   "Detonate a given bomb"
   [arena bomb-id]
-  (let [{{v :v, width :width, height :height, :as grid} :grid,
-         players :players,
-         bombs :bombs,
+  (let [{{:keys [v width height], :as grid} :grid
+         players :players
+         bombs :bombs
          :as arena} arena
-        {[x y, :as coords] :coords} (bomb-id bombs)
+        [x y, :as coords] (bomb-id bombs)
         grid (spread-fire grid coords (fn [[x y]] [(inc x) y]) bomb-radius)
         grid (spread-fire grid coords (fn [[x y]] [(dec x) y]) bomb-radius)
         grid (spread-fire grid coords (fn [[x y]] [x (inc y)]) bomb-radius)
         grid (spread-fire grid coords (fn [[x y]] [x (dec y)]) bomb-radius)
+        grid (dissoc-grid-cell grid coords bomb-id)
+        bombs (dissoc bombs bomb-id)
         arena (assoc arena
-          :bombs (dissoc bombs bomb-id)
+          :bombs bombs
           :grid grid)]
     arena))
 
 (defn eval-arena
   "Check if any bombs should detonate (and detonate in case)"
   [arena timestamp]
-  (let [{{v :v, :as grid} :grid, players :players, bombs :bombs, :as arena} arena]
-    (loop [idx 0 arena arena]
-      (if (= idx (count bombs))
-        arena
-        (let [bomb-id (nth (keys bombs) idx)
-              bomb (bomb-id bombs)
-              arena (if (<= bomb-timeout-ms (- timestamp (:timestamp bomb)))
-                nil
-                arena)]
-          (recur (inc idx) arena))))))
+  (let [{bombs :bombs, :as arena} arena
+        ; update bombs
+        arena (loop [idx 0 {grid :grid, :as arena} arena]
+          (if (= idx (count bombs))
+            arena
+            (let [bomb-id (nth (keys bombs) idx)
+                  bomb-coords (bomb-id bombs)
+                  bomb (bomb-id (cell-at grid bomb-coords))
+                  arena (if (<= bomb-timeout-ms (- timestamp (:timestamp bomb)))
+                    (detonate-bomb arena bomb-id)
+                    arena)]
+              (recur (inc idx) arena))))
+        {grid :grid, players :players} arena
+        ; update players
+        grid (loop [idx 0 grid grid]
+          (if (= idx (count players))
+            grid
+            (let [player-id (nth (keys players) idx)
+                  player-coords (player-id players)
+                  cell (cell-at grid player-coords)
+                  grid (if (contains? cell :fire)
+                    (assoc-grid-cell grid player-coords player-id
+                      (assoc (player-id cell) :hit true))
+                    grid)]
+              (recur (inc idx) grid))))
+        arena (assoc arena :grid grid)]
+      arena
+    ))
 
 (defn -main
   "I don't do a whole lot ... yet."
--- test/bomberman_clj/core_test.clj
+++ test/bomberman_clj/core_test.clj
@@ -2,35 +2,27 @@
   (:require [clojure.test :refer :all]
             [bomberman-clj.core :refer :all]))
 
+(def ts-now 1552767537306)
+
 (deftest test-core
   (testing "An empty (17 x 15) arena with 2 players and 0 bombs should be initialized"
     (let [width 1
           height 2
           players {:player-1 {:glyph \P} :player-2 {:glyph \Q}}
-          arena (init-arena width height players)]
-      (is (not (nil? arena)))
-      (is (map? arena))
-      (is (contains? arena :grid))
-      (let [grid (:grid arena)
-            v (:v grid)]
-        (is (= (* width height) (count v)))
-        (is (= 2 (count (filter (complement nil?) v)))))
-      (is (contains? arena :players))
-      (let [players (:players arena)]
-        (is (map? players))
-        (is (= 2 (count players)))
-        (let [player-1 (:player-1 players)
-              player-2 (:player-2 players)]
-          (is (map? player-1))
-          (is (= \P (:glyph player-1)))
-          (is (contains? player-1 :coords))
-          (is (map? player-2))
-          (is (= \Q (:glyph player-2)))
-          (is (contains? player-2 :coords)))
-      (is (contains? arena :bombs))
-      (let [bombs (:bombs arena)]
-        (is (map? bombs))
-        (is (= 0 (count bombs)))))))
+          arena (init-arena width height players)
+          {{v :v, :as grid} :grid, players :players, bombs :bombs} arena]
+      (is (= (* width height) (count v)))
+      (is (= 2 (count (filter (complement nil?) v))))
+      (is (map? players))
+      (is (= 2 (count players)))
+      (let [coords-1 (:player-1 players)
+            coords-2 (:player-2 players)]
+        (is (vector? coords-1))
+        (is (= 2 (count coords-1)))
+        (is (vector? coords-2))
+        (is (= 2 (count coords-2))))
+      (is (map? bombs))
+      (is (= 0 (count bombs)))))
 
   (testing "3 players should spawn in the correct positions"
     (let [width 17
@@ -52,7 +44,11 @@
   (testing "Grid cells should be identified correctly"
     (let [width 2
           height 3
-          grid {:width width, :height height, :v [0 1 nil 3 4 5]}]
+          grid {:width width
+                :height height
+                :v [0   1
+                    nil 3
+                    4   5]}]
       (is (= 0 (cell-at grid [0 0])))
       (is (= 1 (cell-at grid [1 0])))
       (is (cell-empty? grid [0 1]))
@@ -63,7 +59,11 @@
   (testing "A pair of random coordinates should be found"
     (let [width 2
           height 3
-          grid {:width width, :height height, :v [nil nil nil nil nil nil]}
+          grid {:width width
+                :height height
+                :v [nil nil
+                    nil nil
+                    nil nil]}
           [x y] (rand-coords grid)]
       (is (integer? x))
       (is (integer? y))
@@ -73,7 +73,11 @@
   (testing "Grid cells should be identified correctly"
     (let [width 2
           height 3
-          grid {:width width, :height height, :v [0 1 nil 3 nil 5]}]
+          grid {:width width
+                :height height
+                :v [0   1
+                    nil 3
+                    nil 5]}]
       (is (= 0 (cell-at grid [0 0])))
       (is (= 1 (cell-at grid [1 0])))
       (is (cell-empty? grid [0 1]))
@@ -84,14 +88,20 @@
   (testing "An random empty cell should be found"
     (let [width 3
           height 3
-          grid {:width width, :height height, :v [1 1 1 1 1 1 1 nil 1]}]
+          grid {:width width
+                :height height
+                :v [1 1   1
+                    1 1   1
+                    1 nil 1]}]
       (is (= [1 2] (find-empty-cell grid)))))
 
   (testing "A player can move to NESW empty cells within grid"
-    (let [width 3
-          height 3
-          players {:player-1 {:glyph \P :coords [1 1]}}
-          arena (init-arena width height players)
+    (let [plr {:player-1 {:glyph \P}}
+          v [nil nil nil
+             nil plr nil
+             nil nil nil]
+          players {:player-1 [1 1]}
+          arena {:bombs {} :grid {:width 3 :height 3 :v v} :players players}
           arena (move arena :player-1 :south)
           arena (move arena :player-1 :south)  ; hit wall
           arena (move arena :player-1 :west)
@@ -102,7 +112,7 @@
           arena (move arena :player-1 :north)  ; hit wall
           arena (move arena :player-1 :north)  ; hit wall
           {players :players {v :v} :grid} arena
-          {[x y] :coords} (:player-1 players)]
+          [x y, :as coords] (:player-1 players)]
       (is (= 0 x))
       (is (= 0 y))
       (is (nil? (nth v 4)))))
@@ -118,85 +128,215 @@
       (is (= 0 y))))
 
   (testing "Navigating with invalid direction should fail"
-    (let [coords [1 1]]
-      (is (thrown-with-msg? Exception #"invalid direction:" (navigate coords :down)))))
+    (is (thrown-with-msg? Exception #"invalid direction:" (navigate [0 0] :down))))
 
   (testing "player-1 should place a bomb at in their current position"
-    (let [v [{:glyph \P}]
+    (let [v [{:player-1 {:glyph \P}}]
           arena {:grid {:width 1, :height 1, :v v}
-                :players {:player-1 {:glyph \P, :coords [0 0]}}}
-          {{v :v, :as grid} :grid, bombs :bombs, :as arena} (plant-bomb arena :player-1)
+                 :players {:player-1 [0 0]}}
+          arena (plant-bomb arena :player-1)
+          {{v :v, :as grid} :grid, bombs :bombs, :as arena} arena
           cell (first v)]
       (is (= 1 (count bombs)))
-      (let [bomb ((keyword (str "x" 0 "y" 0)) bombs)]
-        (is (map? bomb))
-        (is (contains? bomb :coords))
-        (is (not (nil? (re-matches #"\d{13}" (str (:timestamp bomb)))))))
-      (is (contains? cell :bomb))
-      (let [bomb (:bomb cell)]
+      (let [bomb (:bomb-x0y0 bombs)]
+        (is (vector? bomb))
+        (is (= 2 (count bomb))))
+      (is (contains? cell :bomb-x0y0))
+      (let [bomb (:bomb-x0y0 cell)]
         (is (map? bomb))
         (is (not (nil? (re-matches #"\d{13}" (str (:timestamp bomb)))))))))
 
   (testing "a planted bomb should still be there after the player moves away"
-    (let [v [{:glyph \P} nil]
+    (let [v [{:player-1 {:glyph \P}} nil]
           arena {:grid {:width 1, :height 2, :v v}
-                  :players {:player-1 {:glyph \P, :coords [0 0]}}}
+                 :players {:player-1 [0 0]}}
           arena (plant-bomb arena :player-1)
-          {{v :v, :as grid} :grid, :as arena} (move arena :player-1 :south)
+          {{v :v, :as grid} :grid, bombs :bombs, :as arena} (move arena :player-1 :south)
           cell (first v)]
-      (is (contains? cell :bomb))
-      (let [bomb (:bomb cell)]
+      (is (= 1 (count bombs)))
+      (is (contains? cell :bomb-x0y0))
+      (let [bomb (:bomb-x0y0 cell)]
         (is (map? bomb))
         (is (not (nil? (re-matches #"\d{13}" (str (:timestamp bomb)))))))))
 
-  (testing "an evaluated arena without any bombs should have no changes"
-    (let [v [nil nil nil nil {:glyph \P} nil nil nil nil]
-          arena {:grid {:width 3, :height 3, :v v}
-                 :players {:player-1 {:glyph \P, :coords [1 1]}
-                 :bombs {}}}
-          evaluated-arena (eval-arena arena (System/currentTimeMillis))]
-      (is (= evaluated-arena arena))))
-
-  (testing "an evaluated arena with an expired bomb should contain detonated bombs"
-    (let [v [{:bomb {:timestamp 0}} nil nil
-             nil {:glyph \P} nil
-             nil nil nil]
-          arena {:grid {:width 3, :height 3, :v v}
-                 :players {:player-1 {:glyph \P, :coords [1 1]}}
-                 :bombs {:x0y0 {:timestamp 0, :coords [0 0]}}}
-          evaluated-arena (eval-arena arena 10000)]
-      (is (not= evaluated-arena arena))))
-
-  (testing "a detonating bomb should spread horizontally and vertically"
-    (let [bom {:bomb {:timestamp 0}}
-          plr {:glyph \P}
-          v [nil nil nil nil
-             nil bom nil nil
-             nil nil plr nil
-             nil nil nil nil]
-          bomb-id :x0y0
-          arena {:grid {:width 4, :height 4, :v v}
-                 :players {:player-1 {:glyph \P, :coords [2 2]}}
-                 :bombs {bomb-id {:timestamp 0, :coords [1 1]}}}
-          {{v :v, :as grid} :grid, bombs :bombs, :as arena} (detonate-bomb arena bomb-id)]
+  (testing "a detonating bomb should spread horizontally and vertically, passing by the offset player"
+    (let [bomb-id :bomb-x1y1
+          bom {bomb-id {:timestamp 0}}
+          plr {:player-1 {:glyph \P}}
+          v [nil nil nil nil nil
+             nil bom nil nil nil
+             nil nil plr nil nil
+             nil nil nil nil nil]
+          arena {:grid {:width 5, :height 4, :v v}
+                 :players {:player-1 [2 2]}
+                 :bombs {bomb-id [1 1]}}
+          {{v :v, :as grid} :grid, bombs :bombs, :as arena}
+            (detonate-bomb arena bomb-id)]
         (is (empty? bombs))
-        (is (not (contains? (nth v 5) :bomb)))
+        (is (not (contains? (nth v 6) bomb-id)))
+        (is (contains? (nth v 12) :player-1))
         (are [cell] (contains? cell :fire)
           (nth v 1)
-          (nth v 4)
           (nth v 5)
-          (nth v 6)
           (nth v 7)
-          (nth v 9)
-          (nth v 13))
+          (nth v 8)
+          (nth v 11)
+          (nth v 16))
         (are [cell] (not (contains? cell :fire))
           (nth v 0)
           (nth v 2)
           (nth v 3)
-          (nth v 8)
+          (nth v 4)
+          (nth v 9)
           (nth v 10)
-          (nth v 11)
           (nth v 12)
           (nth v 14)
-          (nth v 15))))
+          (nth v 15)
+          (nth v 17)
+          (nth v 18)
+          (nth v 19))))
+
+  (testing "a detonating bomb should spread until the nearby player and stop there"
+    (let [bomb-id :bomb-x1y1
+          bom {bomb-id {:timestamp 0}}
+          plr {:player-1 {:glyph \P}}
+          v [nil nil nil nil nil
+             nil bom nil nil nil
+             nil plr nil nil nil
+             nil nil nil nil nil]
+          arena {:grid {:width 5, :height 4, :v v}
+                 :players {:player-1 [1 3]}
+                 :bombs {bomb-id [1 1]}}
+          {{v :v, :as grid} :grid, bombs :bombs, :as arena}
+            (detonate-bomb arena bomb-id)]
+        (is (empty? bombs))
+        (is (not (contains? (nth v 6) bomb-id)))
+        (is (not (nil? (:player-1 (nth v 11)))))
+        (are [cell] (contains? cell :fire)
+          (nth v 1)
+          (nth v 5)
+          (nth v 7)
+          (nth v 8)
+          (nth v 11))
+        (are [cell] (not (contains? cell :fire))
+          (nth v 0)
+          (nth v 2)
+          (nth v 3)
+          (nth v 4)
+          (nth v 9)
+          (nth v 10)
+          (nth v 12)
+          (nth v 14)
+          (nth v 15)
+          (nth v 16)
+          (nth v 17)
+          (nth v 18)
+          (nth v 19))))
+
+  (testing "an evaluated arena without any bombs should have no changes"
+    (let [plr {:player-1 {:glyph \P}}
+          v [nil nil nil
+             nil plr nil
+             nil nil nil]
+          arena {:grid {:width 3, :height 3, :v v}
+                 :players {:player-1 [1 1]}
+                 :bombs {}}
+          evaluated-arena (eval-arena arena ts-now)]
+      (is (= evaluated-arena arena))))
+
+  (testing "an evaluated arena with an expired bomb should contain fire"
+    (let [bomb-id :bomb-x0y0
+          bom {bomb-id {:timestamp 0}}
+          plr {:player-1 {:glyph \P}}
+          v [bom nil nil
+             nil plr nil
+             nil nil nil]
+          arena {:grid {:width 3, :height 3, :v v}
+                 :players {:player-1 [1 1]}
+                 :bombs {bomb-id [0 0]}}
+          {{v :v, :as grid} :grid
+           bombs :bombs
+           :as evaluated-arena} (eval-arena arena ts-now)]
+      (is (not= evaluated-arena arena))
+      (are [cell] (contains? cell :fire)
+        (nth v 0)
+        (nth v 1)
+        (nth v 2)
+        (nth v 3)
+        (nth v 6))
+      (are [cell] (not (contains? cell :fire))
+        (nth v 4)
+        (nth v 5)
+        (nth v 7)
+        (nth v 8))
+      (is (not (contains? (nth v 0) :bomb)))
+      (is (not (contains? (:player-1 (nth v 4)) :hit)))
+      (is (= 0 (count bombs)))))
+
+  (testing "an evaluated arena with an expired bomb should hit the nearby player"
+    (let [bomb-id1 :bomb-x0y0
+          bomb-id2 :bomb-x2y1
+          bm1 {bomb-id1 {:timestamp 0}}
+          bm2 {bomb-id2 {:timestamp 0}}
+          plr {:player-1 {:glyph \P}}
+          v [bm1 nil nil
+             nil nil bm2
+             plr nil nil]
+          arena {:grid {:width 3, :height 3, :v v}
+                 :players {:player-1 [0 2]}
+                 :bombs {bomb-id1 [0 0]
+                         bomb-id2 [2 1]}}
+          {{v :v, :as grid} :grid
+           bombs :bombs
+           :as evaluated-arena} (eval-arena arena ts-now)]
+      (is (not= evaluated-arena arena))
+      (are [cell] (contains? cell :fire)
+        (nth v 0)
+        (nth v 1)
+        (nth v 2)
+        (nth v 3)
+        (nth v 4)
+        (nth v 6)
+        (nth v 5)
+        (nth v 8))
+      (are [cell] (not (contains? cell :fire))
+        (nth v 7))
+      (is (not (contains? (nth v 0) bomb-id1)))
+      (is (not (contains? (nth v 5) bomb-id2)))
+      (is (contains? (:player-1 (nth v 6)) :hit))
+      (is (= 0 (count bombs)))))
+
+  ; (testing "Bomb detonations should propagate to nearby bombs, leaving others unchanged"
+  ;   (let [bm1 {:bomb :x0y0}
+  ;         bm2 {:bomb :x2y0}
+  ;         bm3 {:bomb :x1y2}
+  ;         plr {:player-1 {:glyph \P}}
+  ;         v [bm1 nil bm2
+  ;            nil plr nil
+  ;            nil bm3 nil]
+  ;         arena {:grid {:width 3, :height 3, :v v}
+  ;                :players {:player-1 {:glyph \P, :coords [0 2]}}
+  ;                :bombs {:x0y0 {:timestamp 1000000000000, :coords [0 0]}
+  ;                        :x2y0 {:timestamp 9999999999999, :coords [2 0]}
+  ;                        :x1y2 {:timestamp 9999999999999, :coords [1 2]}}}
+  ;         {{v :v, :as grid} :grid
+  ;           {player-1 :player-1} :players
+  ;           bombs :bombs
+  ;           :as evaluated-arena} (eval-arena arena ts-now)]
+  ;     (is (not= evaluated-arena arena))
+  ;     (are [cell] (contains? cell :fire)
+  ;       (nth v 0)
+  ;       (nth v 1)
+  ;       (nth v 2)
+  ;       (nth v 3)
+  ;       (nth v 4)
+  ;       (nth v 6)
+  ;       (nth v 5)
+  ;       (nth v 8))
+  ;     (are [cell] (not (contains? cell :fire))
+  ;       (nth v 7))
+  ;     (is (not (contains? (nth v 0) :bomb)))
+  ;     (is (not (contains? (nth v 5) :bomb)))
+  ;     (is (contains? player-1 :hit))
+  ;     (is (= 0 (count bombs)))))
 )
