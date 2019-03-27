bd2cb0d1a6c41daf733f0273e00c57bcd9c75339
idleherb <idleherb@posteo.org>
2019-03-27 19:12:03 +0100

    Fix specs

--- src/bomberman_clj/specs.clj
+++ src/bomberman_clj/specs.clj
@@ -1,6 +1,14 @@
 (ns bomberman-clj.specs
   (:require [clojure.spec.alpha :as s]))
 
+(defn valid? [spec obj]
+  (let [valid (s/valid? spec obj)]
+    (when (not valid)
+      (do
+        (println (str "E specs::valid? - obj:" obj))
+        (println (s/explain-str spec obj))))
+    valid))
+
 (def max-grid-width 20)
 (def max-grid-height 20)
 
@@ -12,14 +20,14 @@
 
 (s/def ::timestamp (s/and int? #(> % 0) #(= 13 (count (str %)))))
 (s/def ::hit (s/keys :req-un [::timestamp]))
-(s/def ::player-1 (s/keys :req-un [::glyph] ::opt-un [::coords ::hit]))
-(s/def ::player-2 (s/keys :req-un [::glyph] ::opt-un [::coords ::hit]))
+(s/def ::player-1 (s/keys :req-un [::glyph] :opt-un [::coords ::hit]))
+(s/def ::player-2 (s/keys :req-un [::glyph] :opt-un [::coords ::hit]))
 
 (s/def ::bomb (s/keys :req-un [::timestamp]))
 (s/def ::fire (s/keys :req-un [::timestamp]))
 
-(s/def ::cell (s/keys :opt-un [::bomb ::fire ::player-1 ::player-2]))
-(s/def ::v (s/coll-of (s/nilable ::cell)
+(s/def ::cell (s/nilable (s/keys :opt-un [::bomb ::fire ::player-1 ::player-2])))
+(s/def ::v (s/coll-of ::cell
                       :min-count 1
                       :max-count (* max-grid-width max-grid-height)))
 (s/def ::width (s/and int? #(>= max-grid-width % 1)))
@@ -27,8 +35,5 @@
 (s/def ::grid (s/keys :req-un [::v ::width ::height]))
 
 (s/def ::bombs map?)
-
-(s/def ::player-1 (s/and (s/coll-of int? :kind vector) #(= 2 (count %))))
-(s/def ::player-2 (s/and (s/coll-of int? :kind vector) #(= 2 (count %))))
-(s/def ::players (s/keys :req-un [::player-1] :opt-un [::player-2]))
-(s/def ::arena (s/keys :req-un [::bombs ::grid ::players])
+(s/def ::players map?)
+(s/def ::arena (s/keys :req-un [::bombs ::grid ::players]))
