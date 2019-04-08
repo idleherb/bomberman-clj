d43d8a349bcee86522378ef9132624f289a394f3
idleherb <idleherb@posteo.org>
2019-04-08 17:18:36 +0200

    Add FPS generator

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "The Unlicense"
             :url "https://unlicense.org/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.clojure/core.async "0.4.490"]
                  [clojure-lanterna "0.9.7"]]
   :managed-dependencies [[org.clojure/core.rrb-vector "0.0.13"]
                          [org.flatland/ordered "1.5.7"]]
@@ -11,4 +12,5 @@
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}
              :dev {:dependencies [[midje "1.9.7" :exclusions [org.clojure/clojure]]]
-                   :plugins [[lein-midje "3.2.1"]]}})
+                   :plugins [[lein-cloverage "1.1.1"]
+                             [lein-midje "3.2.1"]]}})
