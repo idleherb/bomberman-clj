125f19fde71ac4d7bb0e1363addb80bc7cb5f39e
idleherb <idleherb@posteo.org>
2019-04-09 16:34:28 +0200

    Update config.clj

--- src/bomberman_clj/config.clj
+++ src/bomberman_clj/config.clj
@@ -1,9 +1,9 @@
 (ns bomberman-clj.config)
 
-(def bomb-count 1)
+(def bomb-count 3)
 (def bomb-radius 3)
 
-(def bomb-timeout-ms 10000)
+(def bomb-timeout-ms 3000)
 
 (def bomb-expiration-ms 1000)
 (def fire-expiration-ms 1000)
