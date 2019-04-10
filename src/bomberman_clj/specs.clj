2fe4c6d6c1cdf966e09bea6569f6bd7e08288c14
idleherb <idleherb@posteo.org>
2019-04-10 13:24:05 +0200

    More tests for test data generator

--- src/bomberman_clj/specs.clj
+++ src/bomberman_clj/specs.clj
@@ -33,13 +33,12 @@
 (s/def ::coords (s/keys :req-un [::x ::y]))
 
 (s/def ::hit (s/keys :req-un [::timestamp]))
-(s/def ::player-1 (s/keys :req-un [::glyph ::bomb-count] :opt-un [::coords ::hit]))
-(s/def ::player-2 (s/keys :req-un [::glyph ::bomb-count] :opt-un [::coords ::hit]))
+(s/def ::player (s/keys :req-un [::glyph ::bomb-count] :opt-un [::coords ::hit]))
 
 (s/def ::bomb (s/keys :req-un [::player-id ::timestamp]))
 (s/def ::fire (s/keys :req-un [::timestamp]))
 
-(s/def ::cell (s/nilable (s/keys :opt-un [::bomb ::fire ::player-1 ::player-2])))
+(s/def ::cell (s/nilable (s/keys :opt-un [::bomb ::fire])))
 (s/def ::v (s/coll-of ::cell
                       :min-count 1
                       :max-count (* max-grid-width max-grid-height)))
--- test/bomberman_clj/test_data_test.clj
+++ test/bomberman_clj/test_data_test.clj
@@ -1,8 +1,19 @@
 (ns bomberman-clj.test-data-test
   (:require [midje.sweet :refer [fact facts =>]]
             [bomberman-clj.specs :as specs]
-            [bomberman-clj.test-data :refer [make-timestamp]]))
+            [bomberman-clj.test-data :refer [make-cell-bomb-p1
+                                             make-cell-p1
+                                             make-cell-wall
+                                             make-player-1
+                                             make-player-2
+                                             make-player-3
+                                             make-timestamp]]))
 
 (facts "about test data"
-  (fact "timestamps conform to their spec"
+  (fact "all data generation functions are spec compliant"
+    (make-cell-p1) => #(specs/valid? ::specs/cell %)
+    (make-cell-wall) => #(specs/valid? ::specs/cell %)
+    (make-player-1) => #(specs/valid? ::specs/player %)
+    (make-player-2) => #(specs/valid? ::specs/player %)
+    (make-player-3) => #(specs/valid? ::specs/player %)
     (make-timestamp) => #(specs/valid? ::specs/timestamp %)))
