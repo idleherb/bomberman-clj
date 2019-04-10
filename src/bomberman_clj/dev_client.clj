c82a8791ea90719778ed39778451a605866f6c3e
idleherb <idleherb@posteo.org>
2019-04-10 17:40:25 +0200

    Add player names to dev client

--- src/bomberman_clj/dev_client.clj
+++ src/bomberman_clj/dev_client.clj
@@ -2,6 +2,7 @@
   (:require [clojure.core.async :as async]
             [bomberman-clj.arena :as arena]
             [bomberman-clj.cells :as cells]
+            [bomberman-clj.grid :as grid]
             [bomberman-clj.specs :as specs]
             [lanterna.screen :as s]))
 
@@ -37,44 +38,53 @@
   [scr width height text]
   (let [x (+ h-margin (int (/ (- (* 2 width) (count text)) 2)))
         y (+ v-margin (int (/ height 2)))]
-    (clear-screen scr (+ (* 2 h-margin) (* 2 width)) (+ (* 2 v-margin) height))
     (s/put-string scr x y text {:fg :white, :bg :black})))
 
 (defn- draw-arena
   [arena scr]
-  (if-let [gameover (:gameover arena)]
-    (let [{{:keys [width height], :as grid} :grid} arena
-          text (if (contains? gameover :winner)
-                 (str "*** " (:winner gameover) " won! ***")
-                 "*** No winner! ***")]
-      (draw-centered-text scr width height text))
-    ; else
-    (do
-      (doseq [[row-idx row] (map-indexed vector (arena-rows arena))]
-        (doseq [[cell-idx cell] (map-indexed vector row)]
-          (let [bomb (cells/cell-bomb cell)
-                fire? (contains? cell :fire)
-                player (cells/cell-player cell)]
-            (s/put-string
-              scr  ; screen
-              (+ h-margin (if (= 0 cell-idx) cell-idx (* 2 cell-idx)))  ; x
-              (+ v-margin row-idx)  ; y
-                (cond
-                  (nil? cell) "."
-                  (some? player) (str (:glyph player))
-                  (some? bomb) "X"
-                  fire? "#"
-                  :else (throw (Exception. (str "invalid cell content: " cell))))  ; string
-              {:fg (cond
-                    (nil? cell) :green
-                    fire? :black
-                    :else :white)
-              :bg (cond
-                    fire? :yellow
-                    (some? bomb) :red
-                    :else :black)}))))  ; options
-      (s/move-cursor scr 100 100)))
-  (s/redraw scr))
+  (let [{{:keys [width height], :as grid} :grid, players :players} arena]
+    (clear-screen scr (+ (* 2 h-margin) (* 2 width)) (+ (* 2 v-margin) height))
+    (if-let [gameover (:gameover arena)]
+      (let [text (if-let [player-id (:winner gameover)]
+              (let [coords (player-id players)
+                    player (grid/player-at grid player-id coords)
+                    name (:name player)]
+                (str "*** " name " wins! ***"))
+              "*** No winner! ***")]
+        (draw-centered-text scr width height text))
+      ; else
+      (do
+        (doseq [[row-idx row] (map-indexed vector (arena-rows arena))]
+          (doseq [[cell-idx cell] (map-indexed vector row)]
+            (let [bomb (cells/cell-bomb cell)
+                  fire? (contains? cell :fire)
+                  player-id (cells/cell-player-id cell)
+                  player (cells/cell-player cell)]
+              (when (some? player)
+                (let [player-idx (Integer/parseInt (second (re-matches #".*?(\d+)" (name player-id))))
+                      x (+ h-margin (* 2 cell-idx))
+                      y (- v-margin 2 (- (count players) player-idx))]
+                  (s/put-string scr x y (:name player) {:fg :black, :bg :white})))
+              (s/put-string
+                scr  ; screen
+                (+ h-margin (* 2 cell-idx))  ; x
+                (+ v-margin row-idx)  ; y
+                  (cond
+                    (nil? cell) "."
+                    (some? player) (str (:glyph player))
+                    (some? bomb) "X"
+                    fire? "#"
+                    :else (throw (Exception. (str "invalid cell content: " cell))))  ; string
+                {:fg (cond
+                      (nil? cell) :green
+                      fire? :black
+                      :else :white)
+                :bg (cond
+                      fire? :yellow
+                      (some? bomb) :red
+                      :else :black)}))))  ; options
+        (s/move-cursor scr 100 100)))
+    (s/redraw scr)))
 
 (defn- key-to-event
   [key]
