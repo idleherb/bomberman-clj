ba0dc68ad61a9fe3f0bd378c99bab0e37f9dd56f
idleherb <idleherb@posteo.org>
2019-05-25 11:26:27 +0200

    Tune frontend actions performance

--- src/bomberman_clj/client/state.cljs
+++ src/bomberman_clj/client/state.cljs
@@ -1,7 +1,9 @@
 (ns bomberman-clj.client.state
-  (:require [reagent.core :as r]))
+  (:require [cljs.core.async :as async
+                             :include-macros true]
+            [reagent.core :as r]))
 
-(defonce state (r/atom {:app {}
+(defonce state (r/atom {:app {:actions-ch (async/chan)}
                         :game nil}))
 
 (defn update-game!
